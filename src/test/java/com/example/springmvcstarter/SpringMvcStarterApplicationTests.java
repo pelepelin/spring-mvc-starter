6cf0460cb8aa91e5101d6c187964d1c112992f02
Konstantin Pelepelin <Konstantin.Pelepelin@gmail.com>
2018-06-14 20:48:00 +0300

    Created Spring Boot Web app by Spring Initializr 2.0.2 with Gradle Groovy DSL.

---
+++ src/test/java/com/example/springmvcstarter/SpringMvcStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springmvcstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvcStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
