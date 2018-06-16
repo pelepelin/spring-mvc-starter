93c6aa379663a3126a12fe65b6d845812719c66a
Konstantin Pelepelin <Konstantin.Pelepelin@gmail.com>
2018-06-16 12:53:01 +0300

    Add functional test
    (cherry picked from commit d48f68e350a1e426b6a29200f466b03d59d1020f)

---
+++ src/test/java/com/example/springmvcstarter/GreetingControllerTest.java
@@ -0,0 +1,50 @@
+package com.example.springmvcstarter;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.regex.Pattern;
+import org.hamcrest.CustomTypeSafeMatcher;
+import org.hamcrest.Matcher;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GreetingController.class)
+public class GreetingControllerTest {
+  private static final Matcher<String> TEXT_HTML_MATCHER =
+      new CustomTypeSafeMatcher<String>("matches text/html;charset=utf-8") {
+        @Override
+        protected boolean matchesSafely(String item) {
+          return Pattern.compile("text/html\\s*;\\s*charset=(?i)utf-8")
+              .matcher(item).matches();
+        }
+      };
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  public void testGreetingReturnsGreetingWhenNoParameter() throws Exception {
+    mvc.perform(get("/").accept(MediaType.TEXT_HTML))
+        .andExpect(status().isOk())
+        .andExpect(header().string("content-type", TEXT_HTML_MATCHER))
+        .andExpect(content().string(containsString("Hello, World")));
+  }
+
+  @Test
+  public void testGreetingReturnsGreetingWithNameParameter() throws Exception {
+    mvc.perform(get("/?name=Bill").accept(MediaType.TEXT_HTML))
+        .andExpect(status().isOk())
+        .andExpect(header().string("content-type", TEXT_HTML_MATCHER))
+        .andExpect(content().string(containsString("Hello, Bill")));
+  }
+}
