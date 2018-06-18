0046af8cdcc2f40fa52d02893d2c5cd49d15ea27
Konstantin Pelepelin <Konstantin.Pelepelin@gmail.com>
2018-06-18 21:32:31 +0300

    Don't use parent pom

--- src/test/java/com/example/springmvcstarter/SpringMvcStarterApplicationTest.java
+++ src/test/java/com/example/springmvcstarter/SpringMvcStarterApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringMvcStarterApplicationTests {
+public class SpringMvcStarterApplicationTest {
 
     @Test
     public void contextLoads() {
