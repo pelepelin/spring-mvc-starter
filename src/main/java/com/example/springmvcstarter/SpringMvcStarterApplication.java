72ebb4d3ca1d2713ec7b549d7fb16659e6d8546b
Konstantin Pelepelin <Konstantin.Pelepelin@gmail.com>
2018-06-18 14:22:11 +0300

    Tabs to spaces

--- src/main/java/com/example/springmvcstarter/SpringMvcStarterApplication.java
+++ src/main/java/com/example/springmvcstarter/SpringMvcStarterApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMvcStarterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcStarterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcStarterApplication.class, args);
+    }
 }
