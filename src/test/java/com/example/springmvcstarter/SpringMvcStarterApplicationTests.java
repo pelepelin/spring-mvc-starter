fc4ce1e7eafc673cfca7ef4c074868a720cdb545
Konstantin Pelepelin <Konstantin.Pelepelin@gmail.com>
2018-06-19 12:33:26 +0300

    Fix test include pattern

--- src/test/java/com/example/springmvcstarter/SpringMvcStarterApplicationTests.java
+++ src/test/java/com/example/springmvcstarter/SpringMvcStarterApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringMvcStarterApplicationTest {
+public class SpringMvcStarterApplicationTests {
 
     @Test
     public void contextLoads() {
