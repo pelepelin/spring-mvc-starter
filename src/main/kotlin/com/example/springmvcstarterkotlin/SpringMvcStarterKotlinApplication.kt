4700c0027a65a392522b2d1e6bf652ac0eed764a
Konstantin Pelepelin <Konstantin.Pelepelin@gmail.com>
2021-04-02 19:28:45 +0300

    Created Spring Boot Web app with Gradle and Kotlin using IDEA Spring Initializr 2.4.4.

---
+++ src/main/kotlin/com/example/springmvcstarterkotlin/SpringMvcStarterKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springmvcstarterkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcStarterKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringMvcStarterKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springmvcstarterkotlin/SpringMvcStarterKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springmvcstarterkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMvcStarterKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
