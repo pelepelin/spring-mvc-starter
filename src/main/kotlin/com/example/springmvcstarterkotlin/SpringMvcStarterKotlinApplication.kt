0534720a7a908581f9eefdb13073c7c28830b627
Konstantin Pelepelin <Konstantin.Pelepelin@gmail.com>
2021-04-03 15:49:19 +0300

    Tabs to spaces

--- src/main/kotlin/com/example/springmvcstarterkotlin/SpringMvcStarterKotlinApplication.kt
+++ src/main/kotlin/com/example/springmvcstarterkotlin/SpringMvcStarterKotlinApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class SpringMvcStarterKotlinApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringMvcStarterKotlinApplication>(*args)
+    runApplication<SpringMvcStarterKotlinApplication>(*args)
 }
